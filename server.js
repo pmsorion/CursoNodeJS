c6f1e72c34da78d29cad5eb46f1238b0bf7bd5aa
Francisco Garcia <garciafran@hotmail.com>
2020-04-23 12:01:49 -0500

    bodyparser

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 //nos permite trabajar y gestionar nuestras peticiones
 const router = express.Router();
 const PUERTO = 3000;
 
 var app = express();
+//se debe usar primero el bodyparser antes que 
+//router o de lo contrario nos arroja undefined
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
 //aqui añadinos router a la aplicacion de express
 app.use(router);
+
 
 router.get('/message', (req, res) => {
     res.send('Lista de mensajes');
 });
 
-router.post('/message', (req, res) => {
-    res.send('Mensaje añadido');
+router.delete('/message', (req, res) => {
+    console.log(req.query);
+    console.log(req.body);
+    res.send(`Mensaje ${req.body.text} añadido correctamente`);
 });
 
 // app.use('/', (req, res) => {
