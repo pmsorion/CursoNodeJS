db4f32e1505fee413e1ff4e984eebd7651d5a508
Francisco Garcia <garciafran@hotmail.com>
2020-04-24 18:57:26 -0500

    MongoDB1: Almacenar y leer datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,22 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+
+db.Promise = global.Promise;
+db.connect('mongodb+srv://db_user:alotWdGOx73njYqE@cluster0-6afsn.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'platzinodejs_db'
+});
+console.log('[db] Conectada con exito');
 
 function addMessage(message) {
-    list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessage() {
-    return list;
+async function getMessage() {
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
